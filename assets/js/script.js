c945b31b10e124c50e8948518cb10f7805a3f931
IuliaMitch <noipasz@gmail.com>
2022-04-13 22:13:38 -0300

    Adicionando HTML dinâmico para mostrar as obras

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+const baseURL = "http://localhost:3000/obras";
+
+async function findAllObras() {
+  const response = await fetch(`${baseURL}/todas-obras`);
+
+  const obras = await response.json();
+
+  obras.forEach(function (obra) {
+    document.querySelector("#obraList").insertAdjacentHTML(
+      "beforeend",
+      `
+    <div class="ObraListaItem">
+          <div>
+            <div class="ObraListaItem__nome">${obra.nome}</div>
+            <div class="ObraListaItem__sinopse">
+              ${obra.sinopse}
+            </div>
+            <div class="ObraListaItem__nota">${obra.nota} <img src="./assets/img/star.svg" alt="estrela" width="14px"></div>
+          </div>
+          <img
+            src="${obra.foto}"
+            alt="Obra ${obra.nome}"
+            class="ObraListaItem__foto"
+          />
+        </div>
+    `
+    );
+  });
+}
+
+async function findByIdObras() {
+  const id = document.querySelector("#idObra").value;
+
+  const response = await fetch(`${baseURL}/obra/${id}`);
+  const obra = await response.json();
+
+  const obraEscolhidaDiv = document.querySelector("#obraEscolhida");
+
+  obraEscolhidaDiv.innerHTML = ` <div class="ObraCardItem">
+  <div>
+    <div class="ObraCardItem__nome">${obra.nome}</div>
+    <div class="ObraCardItem__sinopse">
+      ${obra.sinopse}
+    </div>
+    <div class="ObraCardItem__nota">Nota: ${obra.nota}</div>
+  </div>
+  <img
+    src="${obra.foto}"
+    alt="Obra ${obra.nome}"
+    class="ObraCardItem__foto"
+  />
+</div>`;
+}
+
+findAllObras();
