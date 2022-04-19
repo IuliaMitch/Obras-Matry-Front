54c381f59a1ea8757604831280564aad976c044f
IuliaMitch <noipasz@gmail.com>
2022-04-19 14:40:47 -0300

    Mudança no html dinamico e POST funcional

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,11 @@ async function findAllObras() {
     <div class="ObraListaItem">
           <div>
             <div class="ObraListaItem__nome">${obra.nome}</div>
+            <div class="ObraListaItem__nota">${obra.nota} <img src="./assets/img/star.svg" alt="estrela" width="14px"></div>
+
             <div class="ObraListaItem__sinopse">
               ${obra.sinopse}
             </div>
-            <div class="ObraListaItem__nota">${obra.nota} <img src="./assets/img/star.svg" alt="estrela" width="14px"></div>
           </div>
           <img
             src="${obra.foto}"
@@ -39,10 +40,10 @@ async function findByIdObras() {
   obraEscolhidaDiv.innerHTML = ` <div class="ObraCardItem">
   <div>
     <div class="ObraCardItem__nome">${obra.nome}</div>
+    <div class="ObraCardItem__nota">${obra.nota} <img src="./assets/img/star.svg" alt="estrela" width="14px"></div>
     <div class="ObraCardItem__sinopse">
       ${obra.sinopse}
     </div>
-    <div class="ObraCardItem__nota">${obra.nota} <img src="./assets/img/star.svg" alt="estrela" width="14px"></div>
   </div>
   <img
     src="${obra.foto}"
@@ -54,7 +55,7 @@ async function findByIdObras() {
 
 findAllObras();
 
-function openModalNew () {
+function openModalNew() {
   document.querySelector(".modal-overlay").style.display = "flex";
 }
 
@@ -65,4 +66,49 @@ function closeModalNew() {
   let sinopse = (document.querySelector("#sinopse").value = "");
   let nota = (document.querySelector("#nota").value = "");
   let foto = (document.querySelector("#foto").value = "");
+}
+
+async function createObra() {
+  let nome = document.querySelector("#nome").value;
+  let sinopse = document.querySelector("#sinopse").value;
+  let nota = document.querySelector("#nota").value;
+  let foto = document.querySelector("#foto").value;
+
+  let obra = {
+    nome,
+    sinopse,
+    nota,
+    foto,
+  };
+
+
+  const response = await fetch(`${baseURL}/create`, {
+    method: "post",
+    headers: {
+      "content-Type": "application/json",
+    },
+    mode: "cors",
+    body: JSON.stringify(obra),
+  });
+
+  const newObra = await response.json();
+
+  const html = `<div class="ObraListaItem">
+  <div>
+    <div class="ObraListaItem__nome">${newObra.nome}</div>
+    <div class="ObraListaItem__nota">${newObra.nota} <img src="./assets/img/star.svg" alt="estrela" width="14px"> </div>
+    <div class="ObraListaItem__sinopse">
+      ${newObra.sinopse}
+    </div> 
+  </div>
+  <img
+    src="${newObra.foto}"
+    alt="Obra ${newObra.nome}"
+    class="ObraListaItem__foto"
+  />
+</div>`;
+
+  document.querySelector("#obraList").insertAdjacentHTML("beforeend", html);
+
+  closeModalNew();
 }
